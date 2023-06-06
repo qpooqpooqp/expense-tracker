b1933fc508eeaff5064193a453c615d57ca9c53c
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-06-06 16:06:57 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
-
+const Record = require('./models/record')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(records => res.render('index', { records })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // 設定 port 3000
