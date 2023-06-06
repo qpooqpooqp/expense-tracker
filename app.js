1b16619b253de870f8fc028c127faa09bce05f12
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-06-06 15:51:13 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars');
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
