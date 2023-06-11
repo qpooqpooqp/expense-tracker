479419ad139f065e95686b73046332287aa835bf
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-06-11 20:30:14 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const users = require('./modules/users')
 // 引入 records 模組程式碼
 const records = require('./modules/records')
 router.use('/', home)
 // 將網址結構符合 /records 字串開頭的 request 導向 records 模組
 router.use('/records', records)
+router.use('/users', users)
 module.exports = router
