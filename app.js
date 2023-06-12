758f7eafad203421702596aed9a2a27b40e9c6dd
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-06-12 09:26:43 +0800

    feat: add facebook login &dotnev

--- app.js
+++ app.js
@@ -5,20 +5,23 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const usePassport = require('./config/passport')
 const methodOverride = require('method-override')
-
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
+const PORT = process.env.PORT
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET
   resave: false,
   saveUninitialized: true
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 usePassport(app)
 app.use(flash())
 app.use((req, res, next) => {
@@ -29,6 +32,6 @@ app.use((req, res, next) => {
 })
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const records = require('./modules/records')
 const users = require('./modules/users')
 const home = require('./modules/home')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth') // 掛載 middleware
 router.use('/records', authenticator, records) // 加入驗證程序
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home) // 加入驗證程序
 module.exports = router
